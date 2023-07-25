b265c4d107c66663890826eb28f41225a0f04071
Ubuntu <ubuntu@ip-172-31-92-245.ec2.internal>
2023-07-25 11:20:20 +0000

    chore: update setup_environment.sh

--- setup_environment.sh
+++ setup_environment.sh
@@ -6,21 +6,21 @@ set -e
 # XAUTH=/tmp/.docker.xauth
 # xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-# Download the VOC dataset for INT8 Calibration 
-DATA_DIR=VOCdevkit
-if [ -d "$DATA_DIR" ]; then
-	echo "$DATA_DIR has already been downloaded"
-else
-	echo "Downloading VOC dataset"
-	wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
-	tar -xf VOCtest_06-Nov-2007.tar
-fi
+# # Download the VOC dataset for INT8 Calibration 
+# DATA_DIR=VOCdevkit
+# if [ -d "$DATA_DIR" ]; then
+# 	echo "$DATA_DIR has already been downloaded"
+# else
+# 	echo "Downloading VOC dataset"
+# 	wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
+# 	tar -xf VOCtest_06-Nov-2007.tar
+# fi
 
 echo "Building docker image" 
-docker build -f Dockerfile --tag=object_detection_webcam .
+docker build -f Dockerfile --tag=neilvaltec/tensorrt_object_detection:0.0.1 .
 
 # Start the docker container
 echo "Starting docker container" 
 export CAMERA_SIMULATOR_CONTAINER_ID=$(docker ps -q)
-# docker run --network=container:${CAMERA_SIMULATOR_CONTAINER_ID} --runtime=nvidia -it -v `pwd`:/mnt -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH object_detection_webcam:latest
-docker run --network=container:${CAMERA_SIMULATOR_CONTAINER_ID} --runtime=nvidia -it -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH object_detection_webcam:latest
+# docker run --network=container:${CAMERA_SIMULATOR_CONTAINER_ID} --runtime=nvidia -it -v `pwd`:/mnt -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH neilvaltec/tensorrt_object_detection:0.0.1
+docker run --network=container:${CAMERA_SIMULATOR_CONTAINER_ID} --runtime=nvidia -it -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH neilvaltec/tensorrt_object_detection:0.0.1
